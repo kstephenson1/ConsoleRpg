1a1032d5a023d79ec587a2b7fc83edb4cf4a77a3
Kyle S <kstephenson1@my.wctc.edu>
2025-05-14 23:06:01 -0500

    Fixed heal value

--- ConsoleRpgEntities/Models/Abilities/UnitAbilities/HealAbility.cs
+++ ConsoleRpgEntities/Models/Abilities/UnitAbilities/HealAbility.cs
@@ -20,13 +20,13 @@ public class HealAbility : Ability
             Encounter encounter = new(unit, target);
             if (encounter.IsCrit())
             {
-                Console.WriteLine($"{unit.Name} critically heals {target.Name} for {encounter.Damage + 6} hit points!");
-                target.Damage((encounter.Damage + 6) * -1);
+                Console.WriteLine($"{unit.Name} critically heals {target.Name} for {encounter.Damage} hit points!");
+                target.Damage((encounter.Damage) * -1);
             }
             else if (encounter.IsHit())
             {
-                Console.WriteLine($"{unit.Name} heals {target.Name} for {encounter.Damage + 6} hit points.");
-                target.Damage((encounter.Damage + 6) * -1);
+                Console.WriteLine($"{unit.Name} heals {target.Name} for {encounter.Damage} hit points.");
+                target.Damage((encounter.Damage) * -1);
             }
             else
             {
